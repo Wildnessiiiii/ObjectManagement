c3884110b6606061a468ce9f12e4eca6999cd600
445166392 <445166392@QQ.com>
2022-09-14 00:25:21 +0800

    持久化对象

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,23 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
 
-public class Game : MonoBehaviour
+public class Game : PersistableObject
 {
-    public Transform prefab;
+    public PersistableObject prefab;
     public KeyCode creatKey = KeyCode.C;
     public KeyCode newGameKey = KeyCode.N;
+    public KeyCode saveKey = KeyCode.S;
+    public KeyCode loadKey = KeyCode.L;
 
-    private List<Transform> objects;
-    private string savePath;
+    private List<PersistableObject> objects;
+
+    public PersistentStorage storage;
 
     private void Awake()
     {
-        objects = new List<Transform>();
-        savePath = Path.Combine(Application.persistentDataPath, "saveFile");
-        //Debug.Log(Application.persistentDataPath);
-        //Debug.Log(savePath);
+        objects = new List<PersistableObject>();       
     }
 
     void Start()
@@ -36,11 +34,15 @@ public class Game : MonoBehaviour
         {
             BeginNewGame();
         }
-    }
-
-    void Save()
-    {
-        File.Open(savePath, FileMode.Create);
+        else if (Input.GetKeyDown(saveKey))
+        {
+            Debug.Log("Save");
+            storage.Save(this);
+        }
+        else if (Input.GetKeyDown(loadKey))
+        {
+            storage.Load(this);
+        }
     }
 
     void BeginNewGame()
@@ -54,10 +56,32 @@ public class Game : MonoBehaviour
 
     void CreatObject()
     {
-        Transform t = Instantiate(prefab);
+        PersistableObject o = Instantiate(prefab);
+
+        Transform t = o.transform;
         t.localPosition = Random.insideUnitSphere * 5f;
         t.localRotation = Random.rotation;
         t.localScale = Vector3.one * Random.Range(0.1f, 1f);
-        objects.Add(t);
+        objects.Add(o);
+    }
+
+    public override void Save(GameDataWriter writer)
+    {
+        writer.Write(objects.Count);
+        for(int i =0;i<objects.Count;i++)
+        {
+            objects[i].Save(writer);
+        }
+    }
+
+    public override void Load(GameDataReader reader)
+    {
+        int count = reader.ReadInt();
+        for (int i = 0; i < count; i++)
+        {
+            PersistableObject o = Instantiate(prefab);
+            o.Load(reader);
+            objects.Add(o);
+        }
     }
 }
---
+++ Assets/Scripts/PersistableObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class PersistableObject : MonoBehaviour
+{
+    public virtual void Save(GameDataWriter writer)
+    {
+        writer.Write(transform.localPosition);
+        writer.Write(transform.localRotation);
+        writer.Write(transform.localScale);
+    }
+    //npm config set cache "E:\nodejs\node_cache"
+    //npm config set prefix "E:\nodejs\node_global"
+    public virtual void Load(GameDataReader reader)
+    {
+        transform.localPosition = reader.ReadVector3();
+        transform.localRotation = reader.ReadQuaternion();
+        transform.localScale = reader.ReadVector3();
+    }
+}
